cf790fa9ef1a92b89ad11fe3cc7e69522b9b5375
William Wong <compulim@users.noreply.github.com>
2023-01-13 00:59:49 +0000

    Flush

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { render } from 'react-dom';
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
 
 import App from './ui/App';
+
+initializeIcons();
 
 const rootElement = document.getElementById('root');
 
